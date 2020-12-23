d584c42d8dc11903f3f67da083f6523cc38977fc
Martin Hunt <martinseanhunt@gmail.com>
2020-12-23 07:40:23 +0000

    remove comments - FIN

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,7 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = 'en22y4wdzb'
 export const apiEndpoint = `https://${apiId}.execute-api.eu-west-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'martinseanhunt.eu.auth0.com', // Auth0 domain
   clientId: 'MZ2gPjvyLJNMFIPZQVoL0ZQa4KmKdzCN', // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
